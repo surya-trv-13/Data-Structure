15b0f126ed1a0e9a6ad56843ffee6d059dd7cee7
SURYANARAYAN RATH <suryanarayanrath74@gmail.com>
2019-10-09 14:01:44 +0530

    Changes in the Linked List file

--- Linked List/LInkedList.c
+++ Linked List/LInkedList.c
@@ -22,7 +22,7 @@ void initialise(){
     node[SIZE-1].next = -1;     //Final node will be -1 which will detect Overflow condition.
 }
 
-int getNode(){
+int getNode(){                  // Getting Available node for inserting the data
     int q;
     if(avail == -1){
         printf("Over Flow !!");
@@ -34,7 +34,7 @@ int getNode(){
     return q;
 }
 
-void createList(){
+void createList(){                // Initial creation of the list before doing any operation
     char wish;
     int info,nodePosition = 0;
 
@@ -50,7 +50,7 @@ void createList(){
     }while(wish == 'Y' || wish == 'y');
 }
 
-int insertNode(int info,int nodePostion){
+int insertNode(int info,int nodePostion){       //Insert node code
     int q;
     q = getNode();
 
@@ -60,7 +60,7 @@ int insertNode(int info,int nodePostion){
     return q;
 }
 
-void deleteNode(int position){
+void deleteNode(int position){                  //Deleting node code
     int q;
     position--;
 
@@ -76,13 +76,13 @@ void deleteNode(int position){
     freeNode(q);
 }
 
-void freeNode(int position){
+void freeNode(int position){                //Freeing the position from where the data has been deleted and added to available
     node[position].next = avail;
     avail = position;
     return;
 }
 
-void showNode(){
+void showNode(){                            // Displaying Nodes in the linked List
     int position = 0;
     printf("\n Position    Info    Next-Position");
 
