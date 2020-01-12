e6710f09ba526714188743b78e49aa9efcc04bee
SURYANARAYAN RATH <suryanarayanrath74@gmail.com>
2020-01-12 15:55:57 +0530

    Deleted close file

---
+++ Queue/Using Linked List/QueueLinkedList.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include "Structure.h"
+#include "Push.h"
+#include "Pop.h"
+#include "Peek.h"
+
+void main(){
+    char wish = 'y',choice;
+    do{
+        printf("\n\tEnter Your Choice :"
+            "\n\t\ta. INSERT"
+            "\n\t\tb. DELETE"
+            "\n\t\tc. PEEK"
+            "\n\t\tYour Choice :");
+        scanf("\n%c",&choice);
+        switch(choice){
+            case 'a' : insertQ();
+                break;
+            case 'b' : deleteQ();
+                break;
+            case 'c' : peekQ();
+                break;
+            default : printf("\n\tEnter a valid Choice !");
+                break;
+        }
+        printf("\n\tDo You want to perform more operation :");
+        scanf("\n%c",&wish);
+    }while(wish == 'Y' || wish == 'y');
+}
---
+++ Queue/Using Linked List/Structure.h
@@ -0,0 +1,11 @@
+#ifndef STRUCTURE_H_INCLUDED
+#define STRUCTURE_H_INCLUDED
+
+struct Queue{
+    int frontSide, rearSide;
+    struct Queue *
+};
+
+
+
+#endif // STRUCTURE_H_INCLUDED
