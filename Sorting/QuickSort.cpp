3ab6806381297fdec54240343dbf2db75d288d2c
SURYANARAYAN RATH <suryanarayanrath74@gmail.com>
2020-02-02 12:53:53 +0530

    Added the Quick Sort Algo

---
+++ Sorting/QuickSort.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+using namespace std;
+
+int partition(int a[] , int p , int r){
+    int i = p-1;
+    int j = r+1;
+    int x = a[p];
+    int temp;
+
+    while(1){
+        do{
+            ++i;
+        }while(a[i] < x);
+
+        do{
+            --j;
+        }while(a[j] > x);
+
+        if(i < j){
+            temp = a[i];
+            a[i]= a[j];
+            a[j] = temp;
+        }
+        else{
+         return(j);
+        }
+    }
+
+}
+
+void quickSort(int a[] , int p , int r){
+    int q;
+    if(p < r){
+        q = partition(a , p , r);
+        cout<<"\n\tp = "<<p<<"\t q = "<<q;
+        quickSort(a, p ,q);
+        cout<<"\n\tq+1 = "<<q+1<<"\t r = "<<r;
+        quickSort(a , q+1 , r);
+    }
+}
+
+int main(){
+    int sizeOfArray;
+    cout<<"\t*******SELECTION SORT********\n";
+    cout<<"\tEnter the size of the Array :";
+    cin>>sizeOfArray;
+    int a[sizeOfArray];
+    cout<<"\n\tEnter "<<sizeOfArray<<" elements in the array :";
+    for(int i = 0; i < sizeOfArray; i++){
+        cin>>a[i];
+    }
+
+    quickSort(a , 0 , sizeOfArray-1);
+
+    cout<<"\n\tSorted Array is ";
+    for(int i = 0 ; i< sizeOfArray ; i++){
+        cout<<a[i]<<" ";
+    }
+
+    return 0;
+}
