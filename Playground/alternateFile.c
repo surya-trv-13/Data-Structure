e0d7eee9246b324b5f5ee47d469f0ba98923b55a
SURYANARAYAN RATH <45998114+surya-trv-13@users.noreply.github.com>
2020-09-20 13:27:09 +0000

    Updated ⚽🥇🥊

--- Playground/alternateFile.c
+++ Playground/alternateFile.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
 
 void main(){
-    char ch[100];
-    scanf("%s", ch);
+    int array[5];
+
+    for(int i = 0 ; i < 6 ; i++){
+        scanf("%d", &array[i]);
+    }
+    
+    for(int i = 0 ; i < 6 ; i++){
+        printf("%d", array[i]); 
+    }
 }
